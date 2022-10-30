b87392a6587bc801175d83afa9dba6245285f4fe
dalamarMajere <vitastudio01@gmail.com>
2022-10-30 10:52:39 +0100

    Adjusted namespaces

---
+++ Assets/Scripts/Timer/TimerController.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Timer
+{
+    public class TimerController : MonoBehaviour
+    {
+        [SerializeField] private float maxTime;
+        private float _currentTime;
+
+        private Image _image;
+        private bool isRunning;
+
+        public event Action OnTimerEnded;
+    
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+        }
+
+        private void Update()
+        {
+            if (!isRunning) return;
+
+            if (_currentTime >= maxTime)
+            {
+                Notify();
+                StopTimer();
+            }
+
+            _currentTime += Time.deltaTime;
+            _image.fillAmount = _currentTime / maxTime;
+        }
+
+        public void StartTimer()
+        {
+            _currentTime = 0;
+            isRunning = true;
+        }
+
+        public void StopTimer()
+        {
+            isRunning = false;
+        }
+
+        private void Notify()
+        {
+            OnTimerEnded?.Invoke();
+        }
+    }
+}
